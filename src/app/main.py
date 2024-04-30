fa40166349f9b2fcd9a73a014f29c8f753686d1e
a-cerma <cermaarjola@gmail.com>
2024-04-30 19:30:04 +0200

    Put the Uploaded Video as payload of API

--- src/app/main.py
+++ src/app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from .routers import exercise_analysis
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 app.include_router(exercise_analysis.router) 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 async def root():
--- webapp/src/views/ExerciseAnalyzer.vue
+++ webapp/src/views/ExerciseAnalyzer.vue
@@ -1,43 +1,74 @@
 <template>
-    <br/>
-    <br/>
-    <v-file-input
-        v-model="files"
-        :show-size="1000"
-        color="#008000"
-        label="File input"
-        placeholder="Select your files"
-        prepend-icon="mdi-file-video"
-        variant="outlined"
-        counter
-        multiple
-  >
-    <template v-slot:selection="{ fileNames }">
-      <template v-for="(fileName, index) in fileNames" :key="fileName">
-        <v-chip
-          v-if="index < 2"
-          class="me-2"
-          color="#008000"
-          size="small"
-          label
-        >
-          {{ fileName }}
-        </v-chip>
+    <main>
+        <br/>
+        <br/>
 
-        <span
-          v-else-if="index === 2"
-          class="text-overline text-grey-darken-3 mx-2"
-        >
-          +{{ files.length - 2 }} File(s)
-        </span>
-      </template>
-    </template>
-  </v-file-input></template>
+        <v-row>
+            <v-file-input
+                v-model="file"
+                width="1000"
+                accept="video/mp4"
+                label="File input"
+                placeholder="Select your files"
+                prepend-icon="mdi-file-video"
+                variant="outlined"
+                @change="handleFileUpload"
+                counter
+                
+            >
+        </v-file-input>
+        <v-btn 
+        :loading="isLoading"
+        height="55" @click="uploadFile">Large Button</v-btn>
+    
+        </v-row>
+
+        <v-row>
+            <v-card color="#181818" style="border: 1px solid #008000;" width="800">
+                Text
+            </v-card>
+        </v-row>
+    </main>
+
+  </template>
+
 
 <script setup>
 import {ref} from 'vue'
+import {launchExerciseAnalyzer} from '../api/launchExerciseAnalyzer'
 
-const files = ref([]) 
+const file = ref([]) 
+const isLoading = ref(false)
+
+function handleFileUpload(event) {
+  const files = event.target.files;
+  if (files.length > 0) {
+    file.value = files[0]; // The first selected file
+  }
+}
+
+async function uploadFile() {
+  if (!file.value) return;
+
+
+  const formData = new FormData();
+  console.log([...formData])
+  formData.append('file', file.value);
+  try{
+    isLoading.value = true
+    console.log('is loading', isLoading.value)
+    await launchExerciseAnalyzer(formData)
+
+  }
+  catch(error){
+    console.error(error)
+  }
+  finally{
+    isLoading.value = false
+  }
+
+}
+
 
 </script>
 
