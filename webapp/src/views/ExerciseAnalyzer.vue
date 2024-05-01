26d7b09ee44477d02ffc943356af2433404afcf4
a-cerma <cermaarjola@gmail.com>
2024-05-01 19:15:47 +0200

    Visualize uploaded File

--- webapp/src/views/ExerciseAnalyzer.vue
+++ webapp/src/views/ExerciseAnalyzer.vue
@@ -4,16 +4,17 @@
         <br/>
         <v-row>
             <v-responsive>
-                <video width="200" height="250" controls>
-                    <source src="../assets/kickback.mp4"   style="max-width: 100%;" class="video" type="video/mp4" >
+                <video ref="videoPlayer" width="auto" height="350" v-if="video" controls>
+                    <!-- <source :src="videoSrc"   style="max-width: 100%;" class="video" type="video/mp4" > -->
                     Your browser does not support the video tag.
                 </video>
             </v-responsive>
         </v-row>
-
+        <br/>
+        <br/>
         <v-row>
             <v-file-input
-                v-model="file"
+                v-model="video"
                 width="1000"
                 accept="video/mp4"
                 label="File input"
@@ -32,8 +33,53 @@
         </v-row>
 
         <v-row>
-            <v-card color="#181818" style="border: 1px solid #008000;" width="800">
-                {{ exerciseAnalysis }}
+            <v-card color="#282828" style="border: 1px solid #007BFF;" width="auto">
+                Color Palette for SmartLift
+
+                ### 1. **Main Color for Logo and Highlighted Content**
+
+                - **Electric Blue (#007BFF)**: dynamic and energetic feel that is perfect for a fitness app. Conveys trust, reliability, and a sense of cutting-edge technology.
+
+                ### 2. **Background Color**
+
+                - **Deep Gray (#282828) OR (#121212, darker)**: sleek and modern look, offering a softer alternative to pure black.
+
+                ### 3. **Body Text Color**
+
+                - **Soft White (#F5F5F5)**: excellent readability against the dark gray background. Less harsh than pure white.
+
+                ### 4. **Accent Colors**
+
+                - **Teal (#17A2B8)**: for secondary buttons and links, providing a harmonious match with the electric blue.
+
+                OR
+
+                - **Electric Orange (#FFA500)**: for calls to action and can energize the design, encouraging user interaction.
+
+                - **Platinum (#E5E4E2)** or **Silver (#C0C0C0)**: for less prominent but necessary elements like secondary text or icons.
+                Color Palette for SmartLift
+
+                ### 1. **Main Color for Logo and Highlighted Content**
+
+                - **Electric Blue (#007BFF)**: dynamic and energetic feel that is perfect for a fitness app. Conveys trust, reliability, and a sense of cutting-edge technology.
+
+                ### 2. **Background Color**
+
+                - **Deep Gray (#282828) OR (#121212, darker)**: sleek and modern look, offering a softer alternative to pure black.
+
+                ### 3. **Body Text Color**
+
+                - **Soft White (#F5F5F5)**: excellent readability against the dark gray background. Less harsh than pure white.
+
+                ### 4. **Accent Colors**
+
+                - **Teal (#17A2B8)**: for secondary buttons and links, providing a harmonious match with the electric blue.
+
+                OR
+
+                - **Electric Orange (#FFA500)**: for calls to action and can energize the design, encouraging user interaction.
+
+                - **Platinum (#E5E4E2)** or **Silver (#C0C0C0)**: for less prominent but necessary elements like secondary text or icons.
             </v-card>
         </v-row>
     </main>
@@ -45,27 +91,40 @@
 import {ref} from 'vue'
 import {launchExerciseAnalyzer} from '../api/launchExerciseAnalyzer'
 
-const file = ref([]) 
+const video = ref() 
+
 const isLoading = ref(false)
 const exerciseAnalysis = ref()
+
+const videoPlayer = ref(null);
 
 function handleFileUpload(event) {
   const files = event.target.files;
   if (files.length > 0) {
-    file.value = files[0]; // The first selected file
+    video.value = files[0]; // The first selected file
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+          videoPlayer.value.src = event.target.result;
+     };
+
+    reader.readAsDataURL(video.value);
   }
+
+
+
+
 }
 
 const uploadFile=async()=>{
-  if (!file.value) return;
+  if (!video.value) return;
 
 
   const formData = new FormData();
-  console.log([...formData])
-  formData.append('file', file.value);
+  formData.append('file', video.value);
+  
   try{
     isLoading.value = true
-    console.log('is loading',formData,  isLoading.value)
     const response = await launchExerciseAnalyzer(formData)
     console.log(response)
     exerciseAnalysis.value = JSON.parse(response)[0].review
