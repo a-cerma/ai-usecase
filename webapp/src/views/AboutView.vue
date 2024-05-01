b26f2302785e34f0da4700493362becf96e8a523
a-cerma <cermaarjola@gmail.com>
2024-05-01 22:37:38 +0200

    About PAge

--- webapp/src/views/AboutView.vue
+++ webapp/src/views/AboutView.vue
@@ -1,15 +1,84 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+ <div class="about">
+    <section>
+      <h2 style="color: #ffffff">Welcome to SmartLift</h2>
+      <p>where your fitness journey is supercharged by the transformative power of Generative AI. At SmartLift, we believe that everyone deserves a personalized approach to fitness and nutrition, tailored to their unique body, goals, and lifestyle. Our cutting-edge app combines the latest advancements in AI technology with deep insights into health and fitness to provide you with a revolutionary tool that not only tracks your progress but also actively helps you achieve your best self.</p>
+    </section>
+
+    <section>
+      <h3 style="color: #ffffff">Our Vision</h3>
+      <p>To revolutionize the fitness landscape by integrating sophisticated AI technology that offers real-time, personalized guidance, making optimal health and peak performance accessible to everyone, regardless of their starting point.</p>
+    </section>
+
+    <section>
+      <h3 style="color: #ffffff">Our Mission</h3>
+      <p>To empower individuals around the world to take control of their fitness and nutrition through AI-driven insights, personalized coaching, and a supportive community that motivates and inspires lasting change.</p>
+    </section>
+
+    <section>
+      <h2 style="color: #ffffff">Features at a Glance</h2>
+      <ul>
+        <li><strong>Exercise Analyzer:</strong> Upload your workout videos and get instant feedback on your form to enhance effectiveness and prevent injuries.</li>
+        <li><strong>Workout Planner:</strong> Input your fitness goals and preferences to receive a bespoke workout plan crafted just for you.</li>
+        <li><strong>Virtual Coach:</strong> Your 24/7 AI coach is ready to answer questions, tweak your workout plan, and offer expert fitness advice whenever you need it.</li>
+        <li><strong>Progress Tracker:</strong> Document your fitness journey with photo uploads and receive detailed analyses on your physical transformation.</li>
+        <li><strong>Meal Scanner:</strong> Get instant nutritional information and tailored feedback by simply snapping a photo of your meals.</li>
+        <li><strong>Diet Designer:</strong> Share your dietary preferences and nutritional needs to receive a customized meal plan that complements your fitness goals.</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2 style="color: #ffffff">Meet the Creators</h2>
+      <div>
+        <h4 style="color: #ffffff">Arjola Cerma</h4>
+        <p>With a robust background in Computer Science from Université de Technologie de Compiègne (UTC) and a Master’s in Electronic Engineering from Universiteti Politeknik i Tiranës, Arjola brings a wealth of knowledge in intelligent systems and IoT solutions. Currently a Software Engineer at Avanade, Arjola’s expertise in integrating technology with user-centric applications makes her an invaluable asset to the SmartLift team.</p>
+      </div>
+      <div>
+        <h4 style="color: #ffffff">Amaury Gelin</h4>
+        <p>A seasoned AI professional, Amaury holds a Master’s degree in Mathematics and Computer Science and an Engineer’s degree in Computer Science from UTC. His recent work focuses on Generative AI and Large Language Models, with a proven track record in developing scalable AI solutions. Amaury’s skills in Python, LangChain, and Azure, combined with his deep understanding of AI applications in fitness, drive the innovative core of SmartLift.</p>
+      </div>
+    </section>
+
+    <section>
+      <h2 style="color: #ffffff">Why Choose SmartLift?</h2>
+      <p>SmartLift isn’t just an app; it’s your personal trainer, nutritionist, and motivator all rolled into one. It’s designed for individuals who are serious about making a change and are looking for a smart, sustainable way to achieve their health and fitness goals. With SmartLift, you step into the future of fitness:</p>
+      <ul>
+        <li><strong>Real-Time Analysis</strong> and feedback during your workouts.</li>
+        <li><strong>Interactive Coaching</strong> that evolves with you.</li>
+        <li><strong>Comprehensive Integration</strong> of both fitness and nutrition for a holistic approach.</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2 style="color: #ffffff">Join Us</h2>
+      <p>Embark on your fitness journey with SmartLift and experience a life where peak physical condition and optimal health are not just possible, but expected. Download SmartLift today and transform your approach to fitness and nutrition, one smart step at a time.</p>
+      <p><strong>SmartLift</strong> – Train Smart, Live Strong.</p>
+    </section>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
+} */
+.about{
+  padding-top: 50px;
+}
+h2{
+  font-weight: bold;
+}
+h3{
+  font-weight: bold;
+}
+h4{
+  font-weight: bold;
+}
+strong{
+  font-weight: bold;
+  color: #FFFFFF;
 }
 </style>
